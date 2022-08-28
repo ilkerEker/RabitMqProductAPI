7e62c9069c7a127d1ac59934bbf5e799b6d3142c
ilker <ilker@LAPTOP-D1GQGR3P>
2022-08-28 23:43:05 +0300

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabitMqProductAPI.Models;
+using RabitMqProductAPI.RabitMQ;
+using RabitMqProductAPI.Services;
+
+namespace RabitMqProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductService productService;
+        private readonly IRabitMQProducer _rabitMQProducer;
+
+        public ProductController(IProductService productService, IRabitMQProducer rabitMQProducer)
+        {
+            this.productService = productService;
+            _rabitMQProducer = rabitMQProducer;
+        }
+        [HttpGet("productlist")]
+        public IEnumerable<Product> ProductList()
+        {
+            var productList = productService.GetProductList();
+            return productList;
+        }
+        [HttpGet("getproductbyid")]
+        public Product GetProductById(int id)
+        {
+            
+            return productService.GetProductById(id);
+        }
+        [HttpPost("addproduct")]
+        public Product AddProduct(Product product)
+        {
+            var productData = productService.AddProduct(product);
+            _rabitMQProducer.SendProductMessage(productData);
+            return productData;
+        }
+    }
+}
